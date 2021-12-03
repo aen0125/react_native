aee8b3cbd8a5b37a3f32969968e2ed0591d338d7
aen0125 <54615050+aen0125@users.noreply.github.com>
2021-12-02 19:35:52 -0500

    Login

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -92,10 +92,12 @@ class Reservation extends Component {
                                     [
                                         {
                                             text: 'Cancel',
-                                            style: 'cancel'
+                                            style: 'cancel',
+                                            onPress: () => this.resetForm()
                                         },
                                         {
-                                            text: 'OK'
+                                            text: 'OK',
+                                            onPress: () => this.resetForm()
                                         }
                                     ],
                                 )
